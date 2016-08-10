87faff95064c9b72cd1879d7868c788563ced068
alnye655321 <alnye655321@gmail.com>
2016-08-10 10:00:54 -0600

    created navbar and moved form into it

--- js/junk.js
+++ js/junk.js
@@ -123,3 +123,52 @@ console.log(nwLatQuadData); console.log(swLatQuadData); console.log(nwLonQuadDat
 
 console.log(quadData);
 //close Fill in quad crime data counts
+
+
+<h1>Denver Crime Tracker</h1>
+<div class="row">
+<form>
+  <div class="form-group">
+   <label for="dayAPI">Select Day:</label>
+   <select class="form-control" id="dayAPI">
+     <option>monday</option>
+     <option>tuesday</option>
+     <option>wednesday</option>
+     <option>thursday</option>
+     <option>friday</option>
+     <option>saturday</option>
+     <option>sunday</option>
+   </select>
+ </div>
+  <div class="form-group">
+   <label for="hourAPI">Select Hour:</label>
+   <select class="form-control" id="hourAPI">
+     <option>00</option>
+     <option>01</option>
+     <option>02</option>
+     <option>03</option>
+     <option>04</option>
+     <option>05</option>
+     <option>06</option>
+     <option>07</option>
+     <option>08</option>
+     <option>09</option>
+     <option>10</option>
+     <option>11</option>
+     <option>12</option>
+     <option>13</option>
+     <option>14</option>
+     <option>15</option>
+     <option>16</option>
+     <option>17</option>
+     <option>18</option>
+     <option>19</option>
+     <option>20</option>
+     <option>21</option>
+     <option>22</option>
+     <option>23</option>
+   </select>
+ </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</div>
